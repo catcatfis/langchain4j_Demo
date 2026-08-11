bf3a7378973a3742c1a04e7c20a4a03af32189ff
catcatfis <lbwnb4444@yeah.net>
2026-08-11 17:45:46 +0800

    RAG测试

--- src/main/java/com/cupk/consultant/config/CommonConfig.java
+++ src/main/java/com/cupk/consultant/config/CommonConfig.java
@@ -3,7 +3,11 @@ package com.cupk.consultant.config;
 import com.cupk.consultant.aiservice.ConsultantService;
 import com.cupk.consultant.respository.RedisChatMemoryStore;
 import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
@@ -56,12 +60,18 @@ public class CommonConfig {
     //构建向量数据库
     @Bean
     public EmbeddingStore store() {
-        List<Document> documents =ClassPathDocumentLoader.loadDocuments("content");
+        //List<Document> documents =ClassPathDocumentLoader.loadDocuments("content");
+        //List<Document> documents  = FileSystemDocumentLoader.loadDocuments("D:\\WorkSpace2025\\consultant\\src\\main\\resources\\content");
+        List<Document> documents =ClassPathDocumentLoader.loadDocuments("content",new ApachePdfBoxDocumentParser());
         //构建向量数据库操作对象
         InMemoryEmbeddingStore store=new InMemoryEmbeddingStore();
+
+        //构建文本分割器对象
+        DocumentSplitter ds = DocumentSplitters.recursive(500, 100);
         //构建一个embeddingstoreIngestor对象，将文本数据切割向量化存储
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
                 .embeddingStore(store)
+                .documentSplitter(ds)
                 .build();
         ingestor.ingest(documents);
         return store;
