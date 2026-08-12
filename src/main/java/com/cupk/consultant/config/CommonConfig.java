dd994d843dcb0d82837ae3c162e0fd335eb1084e
catcatfis <lbwnb4444@yeah.net>
2026-08-12 18:33:24 +0800

    RAG测试

--- src/main/java/com/cupk/consultant/config/CommonConfig.java
+++ src/main/java/com/cupk/consultant/config/CommonConfig.java
@@ -2,6 +2,7 @@ package com.cupk.consultant.config;
 
 import com.cupk.consultant.aiservice.ConsultantService;
 import com.cupk.consultant.respository.RedisChatMemoryStore;
+import dev.langchain4j.community.store.embedding.redis.RedisEmbeddingStore;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
@@ -11,6 +12,7 @@ import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentPa
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
@@ -33,6 +35,11 @@ public class CommonConfig {
     @Autowired
     private RedisChatMemoryStore redisChatMemoryStore;
 
+    @Autowired
+    private EmbeddingModel embeddingModel;
+
+    @Autowired
+    private RedisEmbeddingStore redisEmbeddingStore;
     /*@Bean
     public ConsultantService consultantService() {
         ConsultantService consultantService = AiServices.builder(ConsultantService.class)
@@ -63,27 +70,30 @@ public class CommonConfig {
         //List<Document> documents =ClassPathDocumentLoader.loadDocuments("content");
         //List<Document> documents  = FileSystemDocumentLoader.loadDocuments("D:\\WorkSpace2025\\consultant\\src\\main\\resources\\content");
         List<Document> documents =ClassPathDocumentLoader.loadDocuments("content",new ApachePdfBoxDocumentParser());
-        //构建向量数据库操作对象
-        InMemoryEmbeddingStore store=new InMemoryEmbeddingStore();
+        //构建向量数据库操作对象,内存版本
+        //InMemoryEmbeddingStore store=new InMemoryEmbeddingStore();
 
         //构建文本分割器对象
         DocumentSplitter ds = DocumentSplitters.recursive(500, 100);
         //构建一个embeddingstoreIngestor对象，将文本数据切割向量化存储
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-                .embeddingStore(store)
+                //.embeddingStore(store)
+                .embeddingStore(redisEmbeddingStore)
                 .documentSplitter(ds)
+                .embeddingModel(embeddingModel)
                 .build();
         ingestor.ingest(documents);
-        return store;
+        return redisEmbeddingStore;
     }
 
     //构建向量数据库检索对象
     @Bean
-    public ContentRetriever contentRetriever(EmbeddingStore store) {
+    public ContentRetriever contentRetriever(/*EmbeddingStore store*/) {
         return EmbeddingStoreContentRetriever.builder()
-                .embeddingStore(store)
+                .embeddingStore(redisEmbeddingStore)
                 .minScore(0.5)
                 .maxResults(3)
+                .embeddingModel(embeddingModel)
                 .build();
     }
 }
